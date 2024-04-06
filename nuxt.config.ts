5afe65f5a4af1835c558b2d2e124a37183d24bc9
Lo <l709937065@gmail.com>
2024-04-06 20:22:40 +0800

    chore: add deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
+import wasm from "vite-plugin-wasm"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  modules: ["@unocss/nuxt"],
+
+  css: ["@unocss/reset/tailwind.css", "~/assets/main.css"],
+
+  vite: {
+    plugins: [wasm()],
+  },
 })
